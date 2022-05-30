f53ee4c69c8f4e23878d3d6b3a247672108acc53
KieranSpanner <spannerkieran@gmail.com>
2022-05-30 17:33:25 +1000

    Change 7

--- manage.php
+++ manage.php
@@ -1,39 +1,186 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Submitting Quiz Results</title>
+	<meta charset="utf-8"/>
+	<meta name="description" content="php for manageattempts.html"/>
+	<meta name="keywords" content="PHP, MySql1"/>
+	<meta name="author" content="Jerrold Wong Youn Zhuet" />
+	<link href="styles/style.css" rel="stylesheet"/>
+  
+	<!--Reference to external responsive CSS File-->
+	<link href="styles/responsive.css" rel="stylesheet" media="screen and (max-width: 1024px)"/>
+		
+	<title>Query for server</title>
 </head>
 <body>
-<h1>Submitting Quiz Results</h1>
-<?php
-require_once ("settings.php");
-$conn = @mysqli_connect($host,
-$user,
-$pwd,
-$sql_db
-);
-if (!$conn) {
-    echo "<p>Database connection failure</p>";
-} else {
- 
-$firstname = trim($_POST["GivenName"]);
-$lastname = trim($_POST["FamilyName"]);
-$studentnumber = trim($_POST["StudentID"]);
-$datetime = date("Y/m/d"). date("h:i:sa");
-$sql_table = "attempts";
-$query = "INSERT INTO $sql_table (date_time, first_name, last_name, student_number) VALUES ('$datetime', '$firstname', '$lastname', '$studentnumber')";
-$result = mysqli_query($conn, $query);
 
-if(!$result) {
-    echo "<p class=\"wrong\">Something is wrong with ", $query, "</p>";
-} else {
-    echo "<p class=\"ok\">Successfully added New Car record</p>";
-    mysqli_free_result($result);
-}
-mysqli_close($conn);
-}
-?>    
+	<?php include 'header.inc'; ?>
+  
+<?php
+	require_once("settings.php");
+	$conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+	if(!$conn){
+		echo "<p>Database connection failure</p>";
+	}else{
+		
+		$listattempt = htmlspecialchars(trim($_GET["listattempt"]));
+		if($listattempt == null){
+			$deleteattempt = htmlspecialchars(trim($_GET["deleteattempt"]));
+			
+			if($deleteattempt == null){
+				$changescoreID = htmlspecialchars(trim($_GET["changescoreID"]));
+				$changescorenumber = htmlspecialchars(trim($_GET["changescorenumber"]));
+				$changescorevalue = htmlspecialchars(trim($_GET["changescorevalue"]));
+				if(($changescoreID != null OR $changescorenumber != null OR $changescorevalue != null) AND 
+				($changescoreID == null OR $changescorenumber == null OR $changescorevalue == null)){
+					echo "Please fill in the form correctly.";
+				}
+				
+				if($changescoreID == null AND $changescorenumber == null AND $changescorevalue == null){
+					$querymultichoice = htmlspecialchars(trim($_GET["querymultichoice"]));
+					
+					if($querymultichoice == null){
+						$sortfield = htmlspecialchars(trim($_GET["sortfield"]));
+						$sortorder = htmlspecialchars(trim($_GET["sortorder"]));
+						if(($sortfield != null OR $sortorder != null) AND ($sortfield == null OR $sortorder == null)){
+						 echo "Please fill in the form correctly.";
+						}
+						
+					}
+				}
+			}
+		}
+		if(empty($listattempt) && empty($deleteattempt) && empty($changescoreID) && empty($changescorenumber) && empty($changescorevalue) && empty($querymultichoice) 
+			&& empty($sortfield) && empty($sortorder)){
+			echo 'Please fill in at least one section of the form.';
+		}
+		
+		$sql_table="attempts";
+		$query = "";
+		
+		if($listattempt != null){
+			$query="SELECT * FROM attempts WHERE studentID LIKE '$listattempt'";
+			if($query == null){
+				$listattempt = explode(" ", $listattempt);
+				if($listattempt.count > 2){
+					echo "Please enter only first name and last name";
+				}					
+				$firstname = $listattempt[0];
+				$lastname = $listattempt[1];
+				$query = "SELECT * FROM attempts WHERE firstname LIKE '$firstname' AND lastname LIKE '$lastname'";
+			}	
+		}else if($deleteattempt != null){
+			$query="DELETE FROM attempts WHERE studentID LIKE '$deleteattempt'";
+			
+		}else if($changescoreID != null AND $changescorenumber != null AND $changescorevalue != null){
+			$query="UPDATE attempts SET score = '$changescorevalue' WHERE studentID LIKE '$changescoreID' AND attemptnumber LIKE '$changescorenumber'";
+			
+		}else if($querymultichoice != null){
+			if($querymultichoice == "a"){
+				$query = "SELECT * FROM attempts";
+			}else if($querymultichoice == "b"){
+				$query = "SELECT studentID, firstname, lastname FROM attempts WHERE attemptnumber = 1 AND score = 100";
+			}else if($querymultichoice == "c"){
+				$query = "SELECT studentID, firstname, lastname FROM attempts WHERE attemptnumber = 2 AND score < 50";
+			}
+		}else if($sortfield != null AND $sortorder != null){
+			if($sortorder == "a"){
+				$query = "SELECT * FROM attempts ORDER BY $sortfield";
+			}else if($sortorder == "b"){
+				$query = "SELECT * FROM attempts ORDER BY $sortfield DESC";
+			}
+		}
+		
+		if($query != "")
+		{
+			$result = mysqli_query($conn, $query);
+
+			if(!$result){
+				echo "<p class=\"wrong\">Something is wrong with ", $query, "</p>";
+			} else {
+				if($listattempt != null OR ($sortfield != null AND $sortorder != null)){
+					echo "<table style=\"border:1px solid black\">\n";
+					echo "<tr>\n"
+						."<th scope=\"col\">Student Number</th>\n"
+						."<th scope=\"col\">Attempt Number</th>\n"
+						."<th scope=\"col\">First Name</th>\n"
+						."<th scope=\"col\">Last Name</th>\n"
+						."<th scope=\"col\">Score</th>\n"
+						."<th scope=\"col\">Date Time</th>\n"
+						."</tr>\n";
+					while($row = mysqli_fetch_assoc($result)){
+						echo "<tr>\n";
+						echo "<td>",$row["studentid"],"</td>\n";
+						echo "<td>",$row["attemptnumber"],"</td>\n";
+						echo "<td>",$row["firstname"],"</td>\n";
+						echo "<td>",$row["lastname"],"</td>\n";
+						echo "<td>",$row["score"],"</td>\n";
+						echo "<td>",$row["date_time"],"</td>\n";
+						echo "</tr>\n";
+					}
+					echo "</table>\n";
+					
+				}else if($deleteattempt != null){
+					echo "The student's attempt has been deleted.";
+					
+				}else if($changescoreID != null AND $changescorenumber != null AND $changescorevalue != null){
+					echo "The results have been changed.";
+					
+				}else if($querymultichoice != null AND $querymultichoice == "a"){
+					echo "<table style=\"border:1px solid black\">\n";
+					echo "<tr>\n"
+						."<th scope=\"col\">Student Number</th>\n"
+						."<th scope=\"col\">Attempt Number</th>\n"
+						."<th scope=\"col\">First Name</th>\n"
+						."<th scope=\"col\">Last Name</th>\n"
+						."<th scope=\"col\">Score</th>\n"
+						."<th scope=\"col\">Date Time</th>\n"
+						."</tr>\n";
+					while($row = mysqli_fetch_assoc($result)){
+						echo "<tr>\n";
+						echo "<td>",$row["studentid"],"</td>\n";
+						echo "<td>",$row["attemptnumber"],"</td>\n";
+						echo "<td>",$row["firstname"],"</td>\n";
+						echo "<td>",$row["lastname"],"</td>\n";
+						echo "<td>",$row["score"],"</td>\n";
+						echo "<td>",$row["date_time"],"</td>\n";
+						echo "</tr>\n";
+					}
+					echo "</table>\n";
+					
+				}else if($querymultichoice != null AND ($querymultichoice == "b" OR $querymultichoice == "c")){
+					echo "<table style=\"border:1px solid black\">\n";
+					echo "<tr>\n"
+						."<th scope=\"col\">Student Number</th>\n"
+						."<th scope=\"col\">First Name</th>\n"
+						."<th scope=\"col\">Last Name</th>\n"
+						."</tr>\n";
+					while($row = mysqli_fetch_assoc($result)){
+						echo "<tr>\n";
+						echo "<td>",$row["studentid"],"</td>\n";
+						echo "<td>",$row["firstname"],"</td>\n";
+						echo "<td>",$row["lastname"],"</td>\n";
+						echo "</tr>\n";
+					}
+					echo "</table>\n";
+					
+				}
+			
+				if(!($result == true OR $result == false)){
+					mysqli_free_result($result);
+				}
+			}
+		}
+
+		
+			
+		mysqli_close($conn);
+	}
+?>
+</div>
+</div>
+
+<?php include 'footer.inc';?>
+
 </body>
 </html>
---
+++ manageattempts.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="COS10026: Attempts page" />
+  <meta name="keywords" content="HTML, Forms" />
+  <meta name="author" content="Jerrold Wong Youn Zhuet" />
+  <link href="styles/style.css" rel="stylesheet"/>
+  
+  <!--Reference to external responsive CSS File-->
+  <link href="styles/responsive.css" rel="stylesheet" media="screen and (max-width: 1024px)"/>
+
+  <title>Manage Attempts</title>
+</head>
+<body>
+  
+  <?php include 'header.inc'; ?>
+	<div id="content">
+
+  <?php
+  require_once("settings.php"); //connection info
+
+  $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+  //checks if connection is successful
+  if(!$conn)
+  {
+      //Displays an error message
+      echo "<p>Database connection faliure</p>"; //not in production script
+  }
+  else 
+  {
+    echo "
+  <h4>
+    This page is meant for queries on attempts.
+  </h4>
+  <form action=\"manage.php\" method=\"get\" >
+    <fieldset>
+      <legend class=\"liststudentattempt\">
+        <h4>List Particular Student's Attempt</h4>
+      </legend>
+      <p><label for=\"listattempt\">Student ID/Name </label> 
+        <input type=\"text\" name=\"listattempt\" id=\"listattempt\" pattern=\"[a-zA-Z0-9 ]+\" size=\"15\"/>
+      </p>
+    </fieldset>
+    <fieldset class=\"deleteattempt\">
+      <legend>
+        <h4>Delete All Attempts of a Student</h4>
+      </legend>
+      <p><label for=\"deleteattempt\">Student ID </label> 
+        <input type=\"text\" name=\"deleteattempt\" id=\"deleteattempt\" pattern=\"\d{7,10}\" size=\"10\"/>
+      </p>
+	</fieldset>
+	
+	<fieldset class=\"changescore\">
+	  <legend>
+		<h4>Changing Score for Quiz Attempt</h4>
+	  </legend>
+	  
+	  <p><label for=\"changescoreID\">Student ID </label> 
+        <input type=\"text\" name=\"changescoreID\" id=\"changescoreID\" pattern=\"\d{7,10}\" size=\"10\"/>
+      </p>
+	  <p><label for=\"changescorenumber\">Attempt Number </label> 
+        <input type=\"text\" name=\"changescorenumber\" id=\"changescorenumber\" pattern=\"\d{1}\" size=\"1\" maxlength=\"1\"/>
+      </p>
+	  <p><label for=\"changescorevalue\">Change Score </label> 
+        <input type=\"text\" name=\"changescorevalue\" id=\"changescorevalue\" pattern=\"\d{3}\" size=\"3\" maxlength=\"3\"/>
+      </p>
+	</fieldset>
+	<fieldset class=\"querymultichoice\">
+	  <legend>
+		<h4>List Attempts</h4>
+	  </legend>
+      <p><label for=\"querymultichoice\">Which of the following can a proxy server mask to an end website?<br></label> 
+        <select name=\"querymultichoice\" id=\"querymultichoice\">
+          <option value=\"\" selected>Please Select</option>
+          <option value=\"a\">List all attempts</option>			
+          <option value=\"b\">List all who got 100% on first attempt</option>
+          <option value=\"c\">List all who got less than 50% on second attempt</option>
+        </select>
+      </p>
+	</fieldset>
+	
+	<fieldset class=\"sort\">
+	 <legend>
+		<h4>Sort Attempts</h4>
+	 </legend>
+	 <p><label for=\"sortfield\">Which field would you like to sort by?<br></label>
+		<select name=\"sortfield\" id=\"sortfield\">
+			<option value=\"\" selected>Please Select</option>
+			<option value=\"attempt_id\">Attempt ID</option>			
+			<option value=\"date_time\">Date Time</option>
+			<option value=\"firstname\">First name</option>
+			<option value=\"lastname\">Last name</option>			
+			<option value=\"studentid\">Student ID</option>
+			<option value=\"attemptnumber\">Attempt number</option>
+			<option value=\"score\">Score</option>
+		</select>
+	 </p>
+	 <p><label for=\"sortorder\">How would you like to sort by?<br></label>
+		<select name=\"sortorder\" id=\"sortorder\">
+			<option value=\"\" selected>Please Select</option>
+			<option value=\"a\">Ascending order</option>			
+			<option value=\"b\">Descending order</option>
+		</select>
+	 </p>
+	</fieldset><br><br>
+    
+    <input type= \"submit\" value=\"Check\"/>
+  </form>";
+  }
+  ?>
+</div>
+</div>
+
+<?php include 'footer.inc';?>
+
+</body>
+</html>

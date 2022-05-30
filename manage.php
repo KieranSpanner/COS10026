9affe9cfe151d5c862e27810001b2fc3b852aa26
KieranSpanner <spannerkieran@gmail.com>
2022-05-30 14:48:39 +1000

    mark quiz

--- manage.php
+++ manage.php
@@ -22,9 +22,8 @@ $firstname = trim($_POST["GivenName"]);
 $lastname = trim($_POST["FamilyName"]);
 $studentnumber = trim($_POST["StudentID"]);
 $datetime = date("Y/m/d"), date("h:i:sa");
-$numberattempt += 1
 $sql_table = "attempts";
-$query = "insert into $sql_table (date/time, first_name, last_name, student_number, number_attempt) values ('$datetime', '$firstname', '$lastname', '$studentnumber', '$numberattempt')";
+$query = "insert into $sql_table (date/time, first_name, last_name, student_number) values ('$datetime', '$firstname', '$lastname', '$studentnumber')";
 $result = mysqli_query($conn, $query);
 
 if(!$result) {
--- quiz.php
+++ quiz.php
@@ -6,50 +6,29 @@
     <meta name="keywords" content="HTML, CSS, JavaScript" />
     <meta name="author" content="Kieran Spanner" />
     <link rel="stylesheet" href="styles/style.css" />
-
     <title>Quiz</title>
   </head>
   <body>
-  <?php
-     include 'header.inc';
-  ?>
+
+  <?php include 'header.inc'; ?>
+
     <header> Search Engine Quiz </header>
-    <form method="post" action="manage.php">
+
+    <form method="post" action="markquiz.php" novalidate="novalidate">
       <fieldset>
         <legend>Student Details</legend>
         <p class="marginbottom">
           <label for="StudentID">StudentID</label>
-          <input
-            type="text"
-            name="StudentID"
-            id="StudentID"
-            pattern="\d{7,10}"
-            size="10"
-            required="required"
-            class="white"     
-          /></p>
-        &nbsp;  
+          <input type="text" name="StudentID" id="Studentid" pattern="\d{7,10}" size="10" required="required" class="white"/>
+        </p>
+        &nbsp; 
+
         <p>
           <label for="GivenName">Given Name</label>
-          <input
-            type="text"
-            name="GivenName"
-            id="GivenName"
-            maxlength="15"
-            size="10"
-            required="required"
-            class="white"     
-          />
-          <label for="FamilyName">Family Name</label>
-          <input
-            type="text"
-            name="FamilyName"
-            id="FamilyName"
-            maxlength="15"
-            size="10"
-            required="required"
-                 class="white"     
-          />
+          <input type="text" name="GivenName" id="firstname" maxlength="15" size="10" required="required" class="white"/>
+          
+        <label for="FamilyName">Family Name</label>
+          <input type="text" name="FamilyName" id="lastname" maxlength="15" size="10" required="required" class="white"/>
         </p>
       </fieldset>
 
@@ -57,22 +36,20 @@
         
         <legend>Question 1</legend>
 
-        <p>
-          <label> What year was the first search engine created? </label>
-        </p>
+        <p> <label> What year was the first search engine created? </label> </p>
         
         <p>
-          <label for="Question1.1">1995</label>
-          <input type="radio" id="Question1.1" name="Question 1" value="Incorrect" />
+          <label for="a">1995</label>
+          <input type="radio" id="a" name="question1" value="a" />
           
-          <label for="Question1.2">1986</label>
-          <input type="radio" id="Question1.2" name="Question 1" value="Incorrect" />
+          <label for="b">1986</label>
+          <input type="radio" id="b" name="question1" value="b" />
           
-          <label for="Question1.3">1972</label>
-          <input type="radio" id="Question1.3" name="Question 1" value="Incorrect" />
+          <label for="c">1972</label>
+          <input type="radio" id="c" name="question1" value="c" />
           
-          <label for="Question1.4">1990</label>
-          <input type="radio" id="Question1.4" name="Question 1" value="Correct" /> 
+          <label for="d">1990</label>
+          <input type="radio" id="d" name="question1" value="d" /> 
           </p>
         
       </fieldset>
@@ -80,16 +57,16 @@
       <fieldset>
         <legend>Question 2</legend>
         <p>
-         <label for="Question2">What does WWW mean?</label>
+         <label for="question2">What does WWW mean?</label>
         </p>
         <p>
-          <select name="Question 2" id="Question2" required="required" class="white">
+          <select name="question2" id="question2" required="required" class="white">
             <option value="">Please Select</option>
-            <option value="Correct">Welcome Windows Wall</option>
-            <option value="Correct">Water Wave Way</option>
-            <option value="Correct">World Way Web</option>
-            <option value="Incorrect">World Wide Web</option>
-            <option value="Correct">World Wide Websites</option>
+            <option value="a">Welcome Windows Wall</option>
+            <option value="b">Water Wave Way</option>
+            <option value="c">World Way Web</option>
+            <option value="d">World Wide Web</option>
+            <option value="e">World Wide Websites</option>
           </select>
         </p>
       </fieldset>
@@ -103,23 +80,23 @@
         </p>
         
         <p>
-          <input type="checkbox" id="Question3.1" name="Question 3" value="Correct" />
-          <label for="Question3.1">A database of web documents</label>
+          <input type="checkbox" id="a" name="question3" value="a" />
+          <label for="a">A database of web documents</label>
         </p>
         
         <p>
-          <input type="checkbox" id="Question3.2" name="Question 3" value="Incorrect" />
-           <label for="Question3.2">Databases gather webpages into index</label>
+          <input type="checkbox" id="b" name="question3" value="b" />
+           <label for="b">Databases gather webpages into index</label>
         </p>
         
         <p>    
-          <input type="checkbox" id="Question3.3" name="Question 3" value="Correct" />
-          <label for="Question3.3">A search engine operating on that database</label>
+          <input type="checkbox" id="c" name="question3" value="c" />
+          <label for="c">A search engine operating on that database</label>
         </p>
         
         <p>
-          <input type="checkbox" id="Question3.4" name="Question 3" value="Correct" />
-          <label for="Question3.4">A series of programs that determine how search results are displayed</label>
+          <input type="checkbox" id="d" name="question3" value="d" />
+          <label for="d">A series of programs that determine how search results are displayed</label>
         </p>
         
         </p>
@@ -128,27 +105,20 @@
       <fieldset>
         <legend>Question 4</legend>
 
-        <p><label for="question4">What was the first search engine called?</label></p>
+        <p><label for="textquestion">What was the first search engine called?</label></p>
         
         <p>
-          <textarea class="white" id="question4" name="Question 4" rows="4" cols="40" placeholder="Write the anwser to the question here...."></textarea>
+          <textarea class="white" id="textquestion" name="question4" rows="4" cols="40" placeholder="Write the anwser to the question here...."></textarea>
         </p>
       </fieldset>
     
     <fieldset>
         <legend>Question 5</legend>
         <p>
-          <label for="Question5">Is a search engine the same as a web browser? (Y or N answer only)</label>
+          <label for="a">Is a search engine the same as a web browser? (Y or N answer only)</label>
       </p>
       <p>
-         <input
-            type="text"
-            name="Question 5"
-            id="Question5"
-            maxlength="1"
-            required="required"
-            class="white"     
-          />
+         <input type="text" name="question5" id="a" maxlength="1" required="required" class="white"/>
         </p>
       </fieldset>
 
